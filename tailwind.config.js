69ce318eeecd2e2a57a041c7032d55672911b69c
Gaurav Chaulagain <talktogaurav07@gmail.com>
2024-11-12 19:20:24 +0545

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-import defaultTheme from "tailwindcss/defaultTheme";
-
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./resources/**/*.blade.php", "./resources/**/*.js"],
     theme: {
         extend: {
-            fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            colors: {
+                black: "#060606",
             },
         },
     },
