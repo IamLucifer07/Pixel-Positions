9ff660e509411202f62ceda5380357c5e937ce86
Gaurav Chaulagain <talktogaurav07@gmail.com>
2025-02-06 16:51:22 +0545

    image fixed

--- resources/views/Components/employer-logo.blade.php
+++ resources/views/Components/employer-logo.blade.php
@@ -1,3 +1,5 @@
 @props(['employer', 'width' => 90])
 
-<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}">
+{{-- <img src="{{ asset('https://picsum.photos/seed/picsum/200/300') }}" alt="" class="rounded-xl" width="{{ $width }}">
+--}}
+<img src="{{ asset("https://picsum.photos/200/300?random=2") }}" alt="" class="rounded-xl" width="{{ $width }}">
